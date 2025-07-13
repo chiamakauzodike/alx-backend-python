e07550b91791486745c389a184aaac3bfec015c2
Chi <chiamakauzodike@users.noreply.github.com>
2025-07-13 08:34:47 +0100

    adding the async methods update

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -5,30 +5,34 @@ import os
 
 DB_NAME = os.environ.get("DB_NAME")
 
-
 async def async_fetch_users():
     async with aiosqlite.connect(DB_NAME) as db:
         cursor = await db.execute("SELECT * FROM users")
         rows = await cursor.fetchall()
-        print("[All Users]")
-        for row in rows:
-            print(row)
         await cursor.close()
+        return rows
 
 async def async_fetch_older_users():
     async with aiosqlite.connect(DB_NAME) as db:
         cursor = await db.execute("SELECT * FROM users WHERE age > 40")
         rows = await cursor.fetchall()
-        print("[Users older than 40]")
-        for row in rows:
-            print(row)
         await cursor.close()
+        return rows
 
 async def fetch_concurrently():
-    await asyncio.gather(
+    all_users, older_users = await asyncio.gather(
         async_fetch_users(),
         async_fetch_older_users()
     )
+    return all_users, older_users
 
-# Run the concurrent fetch
-asyncio.run(fetch_concurrently())
+# Run and get results
+all_users, older_users = asyncio.run(fetch_concurrently())
+
+print("[All Users]")
+for user in all_users:
+    print(user)
+
+print("\n[Users older than 40]")
+for user in older_users:
+    print(user)
