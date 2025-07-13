579b0643fc9fac93d4e61eb9b11f870c6141cef1
Chi <chiamakauzodike@users.noreply.github.com>
2025-07-13 08:01:30 +0100

    adding python decorator time stamp to log

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,14 +1,17 @@
 import sqlite3
 import functools
+from datetime import datetime
 
 #### decorator to log SQL queries
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = kwargs.get('query') if 'query' in kwargs else args[0] if args else None
-        print(f"[LOG] Executing SQL query: {query}")
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{timestamp}] Executing SQL query: {query}")
         return func(*args, **kwargs)
     return wrapper
+     
 
 @log_queries
 def fetch_all_users(query):
